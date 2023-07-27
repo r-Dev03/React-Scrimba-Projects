c3776dd5654336046de0371f81080438f34568cb
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-07-27 16:44:33 -0500

    General markup

---
+++ react-info/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "../src/components/Navbar"
+import Main from "../src/components/Main"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
