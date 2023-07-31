ac9cbbd7140f2791b987a430006170a23a980296
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-07-30 20:14:50 -0500

    General improvements, static site completed without images.

--- react-info/src/components/Navbar.js
+++ react-info/src/components/Navbar.js
@@ -1,5 +1,10 @@
 import React from "react"
 
-export default function Main() {
-    return <h1>Main component</h1>
+export default function Navbar() {
+    return (
+        <nav>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
 }
